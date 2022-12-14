ce0474505052cdf1c1f361542e2f2687069a15f1
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2022-12-14 15:06:32 +0100

    adding a generator toolkit

---
+++ src/utils/generator.go
@@ -0,0 +1,61 @@
+package utils
+
+type generator[T any] struct {
+    next func() (T, bool) // the boolean value returns true when the last element is given
+    stop func()
+
+    returned chan T
+    stopchan chan bool
+
+    nb int
+    stopped bool
+    el T // currently considered element
+}
+
+func (g*generator[T]) Start() {
+    g.returned = make(chan T)
+    g.stopchan = make(chan bool)
+    g.nb = 0
+    g.stopped = false
+
+    go func(){
+        g.nb ++
+        for {
+            g.el, g.stopped = g.next()
+            if g.stopped {
+                return
+            }
+            select {
+            case g.returned <- g.el:
+            case <- g.stopchan:
+                g.stopped = true
+                return
+            }
+        }
+    }()
+}
+
+// returns a slice of all of the remaining elements.
+func (g*generator[T]) Slice() []T {
+    slice := make([]T, 32)
+    slice = append(slice, g.el)
+    for {
+        el, finished := g.next()
+        slice = append(slice, el)
+        if finished {break}
+    }
+    return slice
+}
+
+
+// Returns the next element of the generator, if the generator is finished, returns true. Does return the last element
+func (g*generator[T]) Next() (T, bool){
+    default_t := *(new(T))
+    if g.stopped {
+        return default_t, true
+    } else {
+        el := <- g.returned
+        return el, false
+    }
+
+}
