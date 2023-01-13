e60dfd6c0e1481ad01451378a0c76c78658d3e43
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-13 15:48:33 +0100

    fixed CERES not stopping at CRTL+C

--- CERES.go
+++ CERES.go
@@ -16,30 +16,32 @@ func main() {
 
     conf := config.GetConfig("./var/config/general.yaml")
     pprint.PrintHLine('=')
-    path_to_pipe := conf.Path_to_not + conf.Path_to_pipe
+    path_to_pipe := "var/" + conf.Path_to_pipe
 
     var availableMethods map[string]api.Method = map[string]api.Method{}
 
     pprint.PrintCentered("CERES")
     pprint.PrintHLine('=')
 
-    fmt.Println("To close CERES, press",
-        pprint.Color("[CRTL+C]", "red"))
+    fmt.Println("To close",
+        pprint.Color("CERES", "cyan"), ", press",
+        pprint.Color("[CRTL+C]", "bold red"))
 
     p := api.StartProcess("CERES",
                      availableMethods,
-                     path_to_pipe,
+                     path_to_pipe + "CERES",
                      path_to_pipe + "__in__")
 
     c := make(chan os.Signal, 32)
     signal.Notify(c, os.Interrupt, syscall.SIGTERM)
 
-    for {
-        select {
-        case <-c:
-            p.Stop()
-        case <-p.ListenToClosure():
-        }
+    select {
+    case <-c:
+        fmt.Println("\tCauhgt a CRTL+C, closing")
+        p.Stop()
+        break
+    case <-p.ListenToClosure():
+        break
     }
 
     pprint.PrintHLine('_')
