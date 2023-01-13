61d5ffbd3a7b8a51c95c3d384511db5edf8a9eca
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-13 15:21:28 +0100

    Added print outs

--- CERES.go
+++ CERES.go
@@ -1,19 +1,47 @@
 package main
 
 import (
-    api "github.com/Alex-S-H-P/NOT_API"
+    "os"
+    "os/signal"
+
+    "fmt"
+    "syscall"
+
     "CERES/src/config"
+    pprint "CERES/src/utils/printUtils"
+    api "github.com/Alex-S-H-P/NOT_API"
 )
 
 func main() {
 
     conf := config.GetConfig("/var/config/general.yaml")
+    pprint.PrintHLine('=')
     path_to_pipe := conf.Path_to_not + conf.Path_to_pipe
 
-    var availableMethods map[string]api.Method = make(map[string]api.Method)
+    var availableMethods map[string]api.Method = map[string]api.Method{}
 
-    api.StartProcess("CERES",
+    pprint.PrintCentered("CERES")
+    pprint.PrintHLine('=')
+
+    fmt.Println("To close CERES, press",
+        pprint.Color("[CRTL+C]", "red"))
+
+    p := api.StartProcess("CERES",
                      availableMethods,
                      path_to_pipe,
                      path_to_pipe + "__in__")
+
+    c := make(chan os.Signal, 32)
+    signal.Notify(c, os.Interrupt, syscall.SIGTERM)
+
+    for {
+        select {
+        case <-c:
+            p.Stop()
+        case <-p.ListenToClosure():
+        }
+    }
+
+    pprint.PrintHLine('_')
+    fmt.Println(pprint.Color("Goodbye !", "cyan"))
 }
