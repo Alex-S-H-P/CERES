a5c9fe6bd46c5058df2231c9ebf2b2b20dfb0f48
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-19 23:31:51 +0100

    Adding MERONYMY and HOLONYMY as links (an arm is a part of a body)

--- src/ceres/relations.go
+++ src/ceres/relations.go
@@ -57,3 +57,47 @@ type Link interface {
     // returns a new link that is set.
     set (Entity, Entity) Link
 }
+
+// A is a part of B
+type MERONYMY struct {
+    A Entity
+    B Entity
+}
+
+func (m MERONYMY) reverse() Link {
+    return HOLONYMY{A:m.B, B:m.A}
+}
+
+func (m MERONYMY)GetA() Entity {
+    return m.A
+}
+
+func (m MERONYMY)GetB() Entity {
+    return m.B
+}
+
+func (m MERONYMY) set(A, B Entity) Link {
+    return MERONYMY{A:A, B:B}
+}
+
+// A contains B
+type HOLONYMY struct {
+    A Entity
+    B Entity
+}
+
+func (h HOLONYMY) reverse() Link {
+    return MERONYMY{A:h.B, B:h.A}
+}
+
+func (h HOLONYMY)GetA() Entity {
+    return h.A
+}
+
+func (h HOLONYMY)GetB() Entity {
+    return h.B
+}
+
+func (h HOLONYMY) set(A, B Entity) Link {
+    return HOLONYMY{A:A, B:B}
+}
