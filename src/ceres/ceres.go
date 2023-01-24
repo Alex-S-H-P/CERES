e790aed569b0b6a1344f464e3d6129d9e7caf6b0
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-24 16:36:28 +0100

    minor refactoring on the indentation

--- src/ceres/ceres.go
+++ src/ceres/ceres.go
@@ -1,43 +1,42 @@
 package ceres
 
 type CERES struct {
-    ics ICS
-    pcs PCS
-    ucs UCS
-    ctx *CTX
+	ics ICS
+	pcs PCS
+	ucs UCS
+	ctx *CTX
 
-    root *EntityType
+	root *EntityType
 
-    grammar *grammar
+	grammar *grammar
 
-    initialized bool
+	initialized bool
 
-    sentence_analyser_workers int
-
+	sentence_analyser_workers int
 }
 
-func (c *CERES)Initialize(workers int){
-    c.ics = ICS{}
-    c.ics.Initialize(c)
-    c.pcs = PCS{}
-    c.pcs.Initialize()
-    c.ucs = UCS{}
-    c.ucs.Initialize()
-    c.ctx = new(CTX)
-    c.ctx.Initialize()
+func (c *CERES) Initialize(workers int) {
+	c.ics = ICS{}
+	c.ics.Initialize(c)
+	c.pcs = PCS{}
+	c.pcs.Initialize()
+	c.ucs = UCS{}
+	c.ucs.Initialize()
+	c.ctx = new(CTX)
+	c.ctx.Initialize()
 
-    if c.grammar == nil {
-        c.grammar = new(grammar)
-        c.grammar.groups = make(map[string]group)
-        c.grammar.rules = make([]rule, 0, 1024)
-    }
+	if c.grammar == nil {
+		c.grammar = new(grammar)
+		c.grammar.groups = make(map[string]group)
+		c.grammar.rules = make([]rule, 0, 1024)
+	}
 
-    c.sentence_analyser_workers = 1
+	c.sentence_analyser_workers = 1
 
-    c.createEntityType("entity")
-    c.initialized = true
+	c.createEntityType("entity")
+	c.initialized = true
 
-    c.ucs.ceres_main = c.root
+	c.ucs.ceres_main = c.root
 }
 
 /*
@@ -46,34 +45,38 @@ Creates a valid entityType.
 Adds it to the CERES dictionary.
 
 */
-func (c *CERES)  createEntityType(w Word) {
-    et := c.ics.createEntityType(w)
+func (c *CERES) createEntityType(w Word) {
+	et := c.ics.createEntityType(w)
 
-    if c.root == nil {
-        c.root = et
-    } else {
-        c.root.addChild(et)
-    }
+	if c.root == nil {
+		c.root = et
+	} else {
+		c.root.addChild(et)
+	}
 }
 
 func (c *CERES) createEntityInstance(w Word, et *EntityType) {
-    c.ics.createEntityInstance(w, et)
+	c.ics.createEntityInstance(w, et)
 }
 
-func (c*CERES) allOptions(w Word) []*RecognizedEntity{
-    var total_capacity int
+func (c *CERES) allOptions(w Word) []*RecognizedEntity {
+	var total_capacity int
 
-    icss := c.ics.proposeOptions(w, c.ctx)
-    pcss := c.pcs.proposeOptions(w, c.ctx)
-    ucss := c.ucs.proposeOptions(w, c.ctx)
+	icss := c.ics.proposeOptions(w, c.ctx)
+	pcss := c.pcs.proposeOptions(w, c.ctx)
+	ucss := c.ucs.proposeOptions(w, c.ctx)
 
-    total_capacity = len(icss) + len(pcss) + len(ucss)
+	total_capacity = len(icss) + len(pcss) + len(ucss)
 
-    var options = make([]*RecognizedEntity, 0, total_capacity)
+	var options = make([]*RecognizedEntity, 0, total_capacity)
 
-    options = append(options, icss...)
-    options = append(options, pcss...)
-    options = append(options, ucss...)
+	options = append(options, icss...)
+	options = append(options, pcss...)
+	options = append(options, ucss...)
+
+	return options
+}
+
 
 
     return options
