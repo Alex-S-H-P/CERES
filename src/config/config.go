8f364d7d81f4c6d79b2e8f4358c80d2bec631de9
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-26 21:12:01 +0100

    Adding option field to count workers

--- src/config/config.go
+++ src/config/config.go
@@ -1,27 +1,30 @@
 package config
 
 import (
-    "gopkg.in/yaml.v3"
-    "os"
+	"os"
+
+	"gopkg.in/yaml.v3"
 )
 
 type CERES_Config struct {
-    Version string
+	Version string
 
-    Path_to_not  string
-    Path_to_pipe string
+	Path_to_not  string
+	Path_to_pipe string
+
+	Workers int
 }
 
 func GetConfig(path string) *CERES_Config {
-    file_content, err := os.ReadFile(path)
-    if err != nil {panic(err)}
+	file_content, err := os.ReadFile(path)
+	if err != nil {
+		panic(err)
+	}
 
-
-
-    config := new(CERES_Config)
-    err = yaml.Unmarshal(file_content, config)
-    if err != nil {
-        panic(err)
-    }
-    return config
+	config := new(CERES_Config)
+	err = yaml.Unmarshal(file_content, config)
+	if err != nil {
+		panic(err)
+	}
+	return config
 }
