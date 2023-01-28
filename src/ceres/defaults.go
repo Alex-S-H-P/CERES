8414093bc668023d80e4785440eb725f4eb0ebb3
schoepp <alexandre.schoepp@telecom-sudparis.eu>
2023-01-28 07:49:00 +0100

    writing a bit of code that does not work yet and is therefore commented

---
+++ src/ceres/defaults.go
@@ -0,0 +1,19 @@
+package ceres
+
+/*
+func (c *CERES) DefaultStartUp() {
+	if c.root != nil {
+		panic("Root set.")
+	}
+
+	root, _ := c.addET("entity", nil, group{})
+
+	thing, _ := c.addET("thing", root, group{name: "NOUN_PHYS"})
+	attribute, _ := c.addET("attribute", root, group{name: "ADJ"})
+	concept, _ := c.addET("concept", root, group{name: "NOUN_IDEA"})
+	being, _ := c.addET("being", thing, group{})
+
+	var rules = []rule{}
+
+	c.grammar.rules = rules
+}*/
